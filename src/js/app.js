6feaf56ef079f34738efd3d7bcdd172dbebb3732
AliPea <pearseindeas@gmail.com>
2016-09-14 12:40:32 +0100

    A bit of styling

--- src/js/app.js
+++ src/js/app.js
@@ -120,7 +120,7 @@ Outdoorsy.mapSetup = function() {
     zoom: 13,
     center: new google.maps.LatLng(51.506178,-0.088369),
     mapTypeId: google.maps.MapTypeId.ROADMAP,
-    styles: []
+    styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
   };
 
   this.map = new google.maps.Map(canvas, mapOptions);
@@ -142,9 +142,17 @@ Outdoorsy.createMarkerForActivity = function(index, activity) {
 
   let latlng = new google.maps.LatLng(activity.lat, activity.lng);
 
+  let icon = {
+    url: "/images/badge.png", // url
+    scaledSize: new google.maps.Size(34.95, 40), // scaled size
+    origin: new google.maps.Point(0,0), // origin
+    anchor: new google.maps.Point(0, 0) // anchor
+  };
+
   let marker = new google.maps.Marker({
     position: latlng,
     map: this.map,
+    icon: icon
     // icon: {
     //   url: "http://furtaev.ru/preview/activity_map_pointer_small.png",
     //   scaledSize: new google.maps.Size(56, 56)
